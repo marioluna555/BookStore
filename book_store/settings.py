56a9b6b61164c64a9a2f5521b674c826adfe850e
Mario Luna <marioluna555@gmail.com>
2021-08-29 20:36:42 -0600

    Cambio en puerto bd

--- book_store/settings.py
+++ book_store/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'USER': 'DB_USER',
         'PASSWORD': 'DB_PASSWORD',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '33067',
+        'PORT': '33066',
     }
 }
 
